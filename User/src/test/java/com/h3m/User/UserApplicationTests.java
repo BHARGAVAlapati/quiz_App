60abed62b335242021c5e197548dfd0939da0074
hemanthvaka <hemanthvaka333@gmail.com>
2024-05-05 03:20:11 +0530

    test file in user service

---
+++ User/src/test/java/com/h3m/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h3m.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
